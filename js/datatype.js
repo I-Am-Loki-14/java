3f1c1d791b0f48c87daafc1453d2f3788fb0c77f
Min Naing Oo <minnaingoo792@gmail.com>
2024-07-13 16:52:02 +0630

    java

---
+++ js/datatype.js
@@ -0,0 +1,35 @@
+//*premittive datatype
+String,Number,Boolean,NaN,null,undefined,Symbol
+
+//!("",'',``)
+let a = 100;
+console.log(typeof a);
+
+let aa = "100";
+console.log(typeof aa);
+
+let t = true;
+console.log(typeof t);
+
+let res = 200 === 200;
+console.log(typeof res);
+
+let aaa;
+console.log(typeof aaa);
+
+let n = null;
+console.log(typeof n);
+
+let s = Symbol;
+console.log(typeof s);
+
+let ans  = "200" / 20
+console.log(ans);
+console.log(typeof ans);
+
+//*non-premittive datatype
+let obj = {name:'bobo'};
+console.log(typeof obj);
+
+let arr =[1,2];
+console.log(typeof arr);
---
+++ js/string.js
@@ -0,0 +1,66 @@
+// let name = "maung maung";
+// let age = 20;
+
+// let Intro = "My name is" + name + "and" + "age is" + age;
+// console.log(Intro);
+
+// let Intro2 = `my name is ${name.toUppercase} and is ${age-5}`;
+// console.log(Intro2);
+
+
+// let word = "Hello Myanmar";
+// console.log("Change country:",word.replace("Myanmar","Thai"));
+// console.log("includes:",word.includes("Myanmar"));
+// console.log("inclues:",word.includes("welcome"));
+// console.log("to array",word.split(" "));
+// console.log("hello",word.indexOf("hello"));
+// console.log("Myanmar",word.indexOf("Myanmar"));
+
+
+// console.log("slice==>",word.slice(0,6));
+// console.log("slice==>",word.slice(1,6));
+// console.log("slice==>",word.slice(2,6));
+// console.log("slice==>",word.slice(4,6));
+
+
+
+// console.log("substring==>",word.substring(0,6));
+// console.log("substring==>",word.substring(1,6));
+// console.log("substring==>",word.substring(2,6));
+// console.log("substring==>",word.substring(4,6));
+
+// console.log("substr==>",word.substr(0,6));
+// console.log("substr==>",word.substr(1,6));
+// console.log("substr==>",word.substr(2,6));
+// console.log("substr==>",word.substr(4,6));
+
+
+// let description = " ";
+
+
+
+// let card = `<div class="card">
+//         <h1>Hello</h1>
+//         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi enim dolorem soluta error cumque incidunt
+//             nostrum ex, consectetur officiis maiores amet harum autem! Alias earum vitae facilis quisquam deleniti vel
+//             animi omnis, atque eius culpa.Min Naing Oo,Did you love me</p>
+//             <button>Press</button>
+//     </div>`
+//     document.write(card)
+   
+
+//     let name2 = "nono";
+//     console.log(name2.startsWith("n"));
+//      console.log(name2.endsWith("0"));
+//       console.log(name2.concat("family"));
+
+//       let string = `Lorem ipsum dolor sit amet consectetur my adipisicing elit. Eligendi enim Maung dolorem soluta error cumque incidunt`
+//       console.log(string.search("Maung"));
+
+// Type casting 
+// console.log('==>',typeof String("100"));
+// console.log('==>',Number(200));
+// console.log('==>',Boolean(a));
+
+
+
